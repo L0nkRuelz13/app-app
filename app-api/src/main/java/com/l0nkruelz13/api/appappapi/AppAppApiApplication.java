ba0f3954640126cd49b215fbc40cc8b2d0b478d3
bbw1081 <bbw1081@g.rit.edu>
2026-01-16 11:46:58 -0500

    first commit

---
+++ app-api/src/main/java/com/l0nkruelz13/api/appappapi/AppAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.l0nkruelz13.api.appappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAppApiApplication.class, args);
+	}
+
+}
